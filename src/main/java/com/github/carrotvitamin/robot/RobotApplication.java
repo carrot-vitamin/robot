55d787dcac7cd8faa1f1e10ed99449302dab5dc7
mac <602377321@qq.com>
2019-08-23 14:25:03 +0800

    cloud deploy

--- src/main/java/com/github/carrotvitamin/robot/RobotApplication.java
+++ src/main/java/com/github/carrotvitamin/robot/RobotApplication.java
@@ -2,12 +2,19 @@ package com.github.carrotvitamin.robot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RobotApplication {
+public class RobotApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(RobotApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(RobotApplication.class);
     }
 
 }
